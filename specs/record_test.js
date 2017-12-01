5b4ec67a068dcf85de01e39a67328f661012c538
Jamie Sime <jamiesime@gmail.com>
2017-12-01 14:02:49 +0000

    Passes has artist

--- specs/record_test.js
+++ specs/record_test.js
@@ -3,6 +3,13 @@ var Record = require('../record.js');
 
 describe('Record', function(){
 
+  beforeEach(function(){
+    record1 = new Record("Zola Jesus", "Okiva", "Goth Pop", 1000);
+  });
+
+  it("should have an artist", function(){
+    assert.strictEqual(record1.artist, "Zola Jesus");
+  })
 
 
 });
