fbd6a985a72ead4b90f683cbe8a8502fe4ff8264
Jamie Sime <jamiesime@gmail.com>
2017-12-01 14:05:15 +0000

    Passed has title, has genre and has price

--- specs/record_test.js
+++ specs/record_test.js
@@ -11,5 +11,17 @@ describe('Record', function(){
     assert.strictEqual(record1.artist, "Zola Jesus");
   })
 
+  it("should have a title", function(){
+    assert.strictEqual(record1.title, "Okiva");
+  });
+
+  it("should have a genre", function(){
+    assert.strictEqual(record1.genre, "Goth Pop");
+  });
+
+  it("should have a price", function(){
+    assert.strictEqual(record1.price, 1000);
+  });
+
 
 });
