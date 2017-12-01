81423196beefff8cd80692ef4f6568af5b46c036
Jamie Sime <jamiesime@gmail.com>
2017-12-01 14:18:31 +0000

    Set passing tests for record_store have name and have city

--- specs/record_store_tests.js
+++ specs/record_store_tests.js
@@ -1,11 +1,23 @@
 var assert = require('assert');
-var RecordStore = require(../record_store.js);
+var RecordStore = require("../record_store.js");
 
 describe("RecordStore", function(){
 var store1;
 
-  beforeEach(function{
-    store1 = new RecordStore();
+  beforeEach(function(){
+    store1 = new RecordStore("Bobo Sound", "Bingo City");
   });
+
+  it("should have a name", function(){
+    assert.strictEqual(store1.name, "Bobo Sound");
+  });
+
+  it("should have a city", function(){
+    assert.strictEqual(store1.city, "Bingo City");
+  });
+
+  // it("should have an empty inventory", function(){
+  //
+  // });
 
 });
